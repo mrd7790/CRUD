3c124af277db8c077f07984bad9343a301f7765c
mrd7790 <dolatkhah77@gmail.com>
2022-02-20 14:03:16 +0330

    Initial commit

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.ApiOverview, ),
+    path('create/', views.add_items, name='add_item'),
+    path('all/', views.view_items, name='view_items'),
+    path('update/<int:pk>', views.update_item, name='update_item'),
+    path('item/<int:pk>/delete/', views.delete_item, name='delete-items'),
+]
