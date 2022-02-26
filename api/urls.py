40851f2d1242c8401c180e4b7f14234054347c73
hrsa <hadi@local>
2022-02-26 16:17:52 +0330

    feature/restructure

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,3 @@
-from django.urls import path
+from api.sp.item.urls import item_url_patterns
 
-from . import views
-
-urlpatterns = [
-    path('', views.ApiOverview, ),
-    path('create/', views.add_item, name='add_item'),
-    path('createall/', views.add_items, name='add_items'),
-    path('all/', views.view_items, name='view_items'),
-    path('update/<int:pk>', views.update_item, name='update_item'),
-    path('item/<int:pk>/delete/', views.delete_item, name='delete-items'),
-]
+urlpatterns = [*item_url_patterns]
